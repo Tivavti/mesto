d990d3b11e48ac0693dfbb2fe6132206dddd11dc
Ira Kot <kotelnikovairina@bk.ru>
2023-02-17 10:45:04 +0300

    fix:исправить формат иконок и все комментарии в коде

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,40 @@
 let profileEditButton = document.querySelector('.profile__edit-button');
 let editPopup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__button-closed');
+let userNameElement = document.querySelector('.profile__name');
+let userAboutMeElement = document.querySelector('.profile__occupation');
+let nameInput = document.querySelector('.popup__item_type_name');
+let jobInput = document.querySelector('.popup__item_type_about-me');
+let formElement = document.querySelector('.popup__inputs');
+
 if (!profileEditButton) {
   throw new Error('no edit button');
 };
 
-profileEditButton.addEventListener('click', function() {
-  openPopup(editPopup);
-});
-
-let popupCloseButton = document.querySelector('.popup__button-closed');
-popupCloseButton.addEventListener('click', function () {
-  closePopup(editPopup);
-});
-
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+  nameInput.value = userNameElement.textContent;
+  jobInput.value = userAboutMeElement.textContent;
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-let userNameElement = document.querySelector('.profile__name');
-let userAboutMeElement = document.querySelector('.profile__occupation');
-let nameInput = document.querySelector('.popup__item_name');
-nameInput.value = userNameElement.textContent;
-
-let jobInput = document.querySelector('.popup__item_about-me');
-jobInput.value = userAboutMeElement.textContent;
-
-let formElement = document.querySelector('.popup__inputs');
 function handleFormSubmit(evt) {
     evt.preventDefault();
 userNameElement.textContent = nameInput.value;
 userAboutMeElement.textContent = jobInput.value;
 closePopup(editPopup);
 };
+
+profileEditButton.addEventListener('click', function() {
+  openPopup(editPopup);
+});
+
+
+popupCloseButton.addEventListener('click', function () {
+  closePopup(editPopup);
+});
 
 formElement.addEventListener('submit', handleFormSubmit);
